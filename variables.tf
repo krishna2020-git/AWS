16a0c4d6440ebc43edc563bfe646b515dcb601fc
Krishna <krishnads290#gmail.com>
2025-12-07 17:39:46 +0530

    Update ec2

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
   type        = string
   description = "AWS region to deploy resources"
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "ami_id" {
   type        = string
   description = "AMI ID for the EC2 instance"
-  default     = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI for us-east-1
+  default     = "ami-02b8269d5e85954ef"  # Amazon Linux 2 AMI for us-east-1
 }
 
 variable "instance_type" {
   type        = string
   description = "Instance type for the EC2 instance"
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
