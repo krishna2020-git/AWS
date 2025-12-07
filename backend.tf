26bd5299fea2a43b1d9c07df70d1e3fb42199026
Krishna <krishnads290#gmail.com>
2025-12-07 20:32:01 +0530

    update ci-cd

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-aws-tf-state-bucket"
-    key            = "aws/ec2/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "aws-tf-lock-table"
     encrypt        = true
